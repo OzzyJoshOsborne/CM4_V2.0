513a121918da797a629199f89121ef191251c183
TheWhiteSwordMan <joshuaosborne@sky.com>
2026-07-09 11:58:26 +0100

    BNO086 V1.1 - Bug fixes

--- sensorBNO086.py
+++ sensorBNO086.py
@@ -67,22 +67,26 @@ class SensorBNO086:
 
     def _calucateEulerAngles(self, w, x, y, z):
         #Make sure Quaternion is (w, x, y, z) / (real, i, j, k)
-        #Yaw = atan2(2(wz + xy), 1 - 2(y^2 + z^2))
-        y1 = w * z
-        y2 = x * y
-        yaw = math.atan2(2(y1 + y2), 1 - 2(y^2 + z^2))
-        
-        #Pitch = asin(2(wy - zx))
-        p1 = w * z
-        p2 = z * x
-        pitch = math.asin(2 * (p1 - p2))
+        try:
+            #Yaw = atan2(2(wz + xy), 1 - 2(y^2 + z^2))
+            y1 = w * z
+            y2 = x * y
+            yaw = math.atan2(2.0*(y1 + y2), 1 - 2.0 * ((y)**2 + (z)**2))
+            
+            #Pitch = asin(2(wy - zx))
+            p1 = w * z
+            p2 = z * x
+            print(p1, p2)
+            pitch = math.asin(2.0 * (p1 - p2))
 
-        #Roll = atan2(2(wx + yz), 1 - 2(x^2 + y^2))
-        r1 = w * x
-        r2 = y * z
-        roll = math.atan2(2(r1 + r2), 1 - 2(x^2 + y^2))
+            #Roll = atan2(2(wx + yz), 1 - 2(x^2 + y^2))
+            r1 = w * x
+            r2 = y * z
+            roll = math.atan2(2.0 * (r1 + r2), 1.0 - 2.0 * ((x)**2 + (y)**2))
 
-        return yaw, pitch, roll
+            return yaw, pitch, roll
+        except: 
+            return 0, 0, 0
         
 
     def getSensorData(self):
@@ -102,12 +106,13 @@ class SensorBNO086:
             # mag_x, mag_y, mag_z = self.bno.magnetic
             # print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
             # print("")
-
+            print("===================")
             print("Rotation Vector Quaternion:")
             quat_i, quat_j, quat_k, quat_real = self.bno.quaternion
-            print("I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real))
+            # print("I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real))
             yaw, pitch, roll = self._calucateEulerAngles(quat_real, quat_i, quat_j, quat_k)
-            print(f"Yaw - {yaw} - Pitch - {pitch} - Roll - {roll}")
+            # print(f"Yaw - {yaw} - Pitch - {pitch} - Roll - {roll}")
+            print(f"Yaw - {yaw * 180 / math.pi} - Pitch - {pitch * 180 / math.pi} - Roll - {roll * 180 / math.pi}")
 
 
             return True
@@ -126,7 +131,7 @@ class SensorBNO086:
                 self.getSensorData()
 
                 self.printSensorData()
-                time.sleep(0.2)
+                time.sleep(1)
 
         except OSError as e:
             print(f"Error Reading from sensor BNO086 - {e}")
